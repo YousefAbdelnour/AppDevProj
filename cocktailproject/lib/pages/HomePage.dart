c0fc9fa577d124b4855f7d0562d3f3f92698f24a
PercyPotterson <2156512@edu.vaniercollege.qc.ca>
2024-04-29 15:07:11 -0400

    Home And Ingredient Page now displays api

--- cocktailproject/lib/pages/HomePage.dart
+++ cocktailproject/lib/pages/HomePage.dart
@@ -1,6 +1,7 @@
 import 'dart:ffi';
 
 import 'package:cocktailproject/pages/AboutAppPage.dart';
+import 'package:cocktailproject/pages/IngredientPage.dart';
 import 'package:cocktailproject/pages/LogOutPage.dart';
 import 'package:cocktailproject/pages/LandingPage.dart';
 import 'package:cocktailproject/pages/RegisterPage.dart';
@@ -9,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:lottie/lottie.dart';
 
+import '../ApiManager.dart';
 import '../cocktail.dart';
 
 class HomePage extends StatefulWidget {
@@ -20,12 +22,30 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Color buttonColor = Color(0xFFE0D9CB);
-  var cocktailList;
+  ApiManager api = ApiManager();
+  List<Cocktail> cocktailList = [];
 
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    fetchRandomCocktails();
+  }
+
+  void fetchRandomCocktails() async {
+    try {
+      List<Cocktail> randomCocktails = [];
+      for (int i = 0; i < 5; i++) {
+        var cocktailData = await api.fetchRandomCocktail();
+        var cocktail = Cocktail.fromJson(cocktailData['drinks'][0]);
+        randomCocktails.add(cocktail);
+      }
+      setState(() {
+        cocktailList = randomCocktails;
+      });
+    } catch (e) {
+      // Handle error appropriately
+      print("Error fetching random cocktails: $e");
+    }
   }
 
   @override
@@ -52,27 +72,6 @@ class _HomePageState extends State<HomePage> {
                 width: double.infinity,
                 height: 50, // Adjust height as needed
                 color: Colors.blue,
-                child: Image.asset(
-                  'assets/cocktailHomePage.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
-            // Background Image at the bottom
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                ),
                 child: Image.asset(
                   'assets/cocktailHomePage.jpg',
                   fit: BoxFit.cover,
@@ -115,89 +114,125 @@ class _HomePageState extends State<HomePage> {
                     SizedBox(
                       height: 10,
                     ),
-                    Container(
-                      width: 350,
-                      height: 350,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          image: AssetImage('assets/testCocktail.jpg'), // Replace with your image asset
-                          fit: BoxFit.cover,
-                        ),
-                        border: Border.all(
-                          color: Colors.black, // Border color
-                          width: 1, // Border width
-                        ),
-                          borderRadius: BorderRadius.circular(15)
-                      ),
-
-                      //Listview displaying at least 5 random cocktails
+                    Expanded(
                       child: ListView.builder(
-                        itemCount: cocktailList.length,
+                        itemCount: cocktailList.length, // Number of items in the list
                         itemBuilder: (context, index) {
-                          return Stack(
-                            children: [
-                              Positioned(
-                                child: Padding(
-                                  padding: const EdgeInsets.all(8.0),
-                                  child: Container(
-                                    width: 40,
-                                    height: 40,
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                                    ),
-                                    child: IconButton(
-                                      onPressed: () {
-                                        // Implement bookmark functionality here
-                                      },
-                                      icon: Icon(Icons.bookmark),
-                                      color: Colors.grey,
-                                    ),
+                          return GestureDetector(
+                            onTap: (){
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => IngredientPage(cocktail: cocktailList[index])),
+                              );
+                            },
+                            child: Padding(
+                              padding: const EdgeInsets.only(left:15, right: 15, bottom: 10),
+                              child: Container(
+                                width: 300, // Adjusted width
+                                height: 350,
+                                decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                    image: NetworkImage(cocktailList[index].thumbnail), // Replace with your image asset
+                                    fit: BoxFit.cover,
                                   ),
+                                  border: Border.all(
+                                    color: Colors.black, // Border color
+                                    width: 1, // Border width
+                                  ),
+                                  borderRadius: BorderRadius.circular(15),
                                 ),
-                                right: 0,
-                              ),
-                              Positioned(
-                                bottom: 0,
-                                child: Container(
-                                  width: 350,
-                                  height: 120,
-                                  decoration: BoxDecoration(
-                                    color: Colors.brown,
-                                    border: Border.all(
-                                      color: Colors.black, // Border color set to white
-                                      width: 2, // Border width
+                                // Stack for containing bookmark icon and text
+                                child: Stack(
+                                  children: [
+                                    // Positioned for the bookmark icon
+                                    Positioned(
+                                      child: Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: Container(
+                                          width: 40,
+                                          height: 40,
+                                          decoration: BoxDecoration(
+                                            color: Colors.white,
+                                            borderRadius: BorderRadius.all(Radius.circular(30)),
+                                          ),
+                                          child: IconButton(
+                                            onPressed: () {
+                                              // Implement bookmark functionality here
+                                            },
+                                            icon: Icon(Icons.bookmark),
+                                            color: Colors.grey,
+                                          ),
+                                        ),
+                                      ),
+                                      right: 0,
                                     ),
-                                    borderRadius: BorderRadius.only(
-                                      bottomLeft: Radius.circular(15),
-                                      bottomRight: Radius.circular(15),
-                                    ),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Text(
-                                      "${cocktailList[index].name}\n${cocktailList[index].category}",
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontSize: 30,
+                                    // Positioned for the text at the bottom
+                                    Positioned(
+                                      bottom: 0,
+                                      child: Container(
+                                        width: 363, // Adjusted width
+                                        height: 120,
+                                        decoration: BoxDecoration(
+                                          color: Colors.brown,
+                                          border: Border.all(
+                                            color: Colors.black, // Border color set to white
+                                            width: 2, // Border width
+                                          ),
+                                          borderRadius: BorderRadius.only(
+                                            bottomLeft: Radius.circular(15),
+                                            bottomRight: Radius.circular(15),
+                                          ),
+                                        ),
+                                        child: Padding(
+                                          padding: const EdgeInsets.all(8.0),
+                                          child: Text("${cocktailList[index].name}\n"
+                                              "${cocktailList[index].alcoholic}", // Replace with the cocktail name
+                                            style: TextStyle(
+                                              color: Colors.white,
+                                              fontSize: 30,
+                                            ),
+                                          ),
+                                        ),
                                       ),
                                     ),
-                                  ),
+                                  ],
                                 ),
                               ),
-                            ],
+                            ),
                           );
                         },
-                      )
+                      ),
 
-                    )
+                    ),
 
                   ],
                 ),
               ),
             ),
+            // Background Image at the bottom (now in front of the ListView)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                width: double.infinity,
+                height: 50,
+                decoration: BoxDecoration(
+                  color: Colors.black,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(20),
+                    topRight: Radius.circular(20),
+                  ),
+                ),
+                child: Image.asset(
+                  'assets/cocktailHomePage.jpg',
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
           ],
         ),
+
       ),
     );
   }
--- cocktailproject/lib/pages/IngredientPage.dart
+++ cocktailproject/lib/pages/IngredientPage.dart
@@ -1,14 +1,19 @@
 import 'dart:ffi';
 
+import 'package:cocktailproject/ApiManager.dart';
 import 'package:cocktailproject/pages/AboutAppPage.dart';
 import 'package:cocktailproject/pages/LogOutPage.dart';
 import 'package:cocktailproject/pages/LandingPage.dart';
 import 'package:cocktailproject/pages/RegisterPage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
+import '../cocktail.dart';
+
 class IngredientPage extends StatefulWidget {
-  const IngredientPage({Key? key}) : super(key: key);
+  final Cocktail cocktail; // Add parameter for cocktail ID
+  const IngredientPage({Key? key, required this.cocktail}) : super(key: key);
 
   @override
   State<IngredientPage> createState() => _IngredientPageState();
@@ -16,28 +21,30 @@ class IngredientPage extends StatefulWidget {
 
 class _IngredientPageState extends State<IngredientPage> {
   Color buttonColor = Color(0xFFE0D9CB);
-  bool _selected = false;
-  List<String> ingredients = [
-    'Ingredient 1',
-    'Ingredient 2',
-    'Ingredient 3',
-    'Ingredient 4',
-    'Ingredient 5',
-    'Ingredient 6',
-    'Ingredient 7',
-    'Ingredient 8',
-    'Ingredient 9'
-    // Add more ingredients as needed
-  ];
-
   List<bool> _checkState = [];
-
+  ApiManager api = ApiManager();
+  late Cocktail cocktail;
+  int lenghtOfIng = 0;
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
-    _checkState = List.generate(ingredients.length, (index) => false);
+    fetchCocktailDetails();
   }
+
+  void fetchCocktailDetails() async {
+      cocktail = widget.cocktail;
+      //the api returns ingredients with null value, find the cutoff index
+      for(int i = 0; i < cocktail.ingredients.length; i++){
+        if(cocktail.ingredients[i] == null){
+          lenghtOfIng = i;
+          break;
+        }
+      }
+      setState(() {
+        _checkState = List.generate(cocktail.ingredients.length, (index) => false);
+      });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,7 +60,6 @@ class _IngredientPageState extends State<IngredientPage> {
         )),
         child: Stack(
           children: [
-
             // Background Image at the top
             Positioned(
               top: 0,
@@ -70,27 +76,6 @@ class _IngredientPageState extends State<IngredientPage> {
               ),
             ),
 
-            // Background Image at the bottom
-            Positioned(
-              bottom: 0,
-              left: 0,
-              right: 0,
-              child: Container(
-                width: double.infinity,
-                height: 50,
-                decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20),
-                    topRight: Radius.circular(20),
-                  ),
-                ),
-                child: Image.asset(
-                  'assets/cocktailHomePage.jpg',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ),
             Center(
               child: Padding(
                 padding: const EdgeInsets.only(top: 80),
@@ -99,7 +84,7 @@ class _IngredientPageState extends State<IngredientPage> {
                     //Name Of Drink
                     Flexible(
                       child: Text(
-                        'Coco for coconut',
+                        cocktail.name,
                         style: TextStyle(
                           fontSize: 45.0,
                           fontWeight: FontWeight.bold,
@@ -118,18 +103,18 @@ class _IngredientPageState extends State<IngredientPage> {
                     SizedBox(
                       height: 10,
                     ),
+
                     //DISPLAY IMAGE
                     ClipRRect(
-                        borderRadius: BorderRadius.circular(15),
-                        child: Image.asset(
-                          'assets/testCocktail.jpg',
-                          fit: BoxFit.cover,
-                          width: 330,
-                          height: 320,
-                        )),
-                    SizedBox(
-                      height: 10,
+                      borderRadius: BorderRadius.circular(15),
+                      child: Image.network(
+                        cocktail.thumbnail,
+                        fit: BoxFit.cover,
+                        width: 330,
+                        height: 320,
+                      ),
                     ),
+
                     Padding(
                       padding: const EdgeInsets.all(1.0),
                       child: Text(
@@ -152,7 +137,7 @@ class _IngredientPageState extends State<IngredientPage> {
                     Expanded(
                       child: Scrollbar(
                         child: ListView.builder(
-                          itemCount: ingredients.length,
+                          itemCount: lenghtOfIng,
                           itemBuilder: (context, index) {
                             return CheckboxListTile(
                               value: _checkState[index],
@@ -161,7 +146,7 @@ class _IngredientPageState extends State<IngredientPage> {
                                   _checkState[index] = value ?? false;
                                 });
                               },
-                              title: Text(ingredients[index], style: TextStyle(
+                              title: Text(cocktail.ingredients[index] ?? '', style: TextStyle(
                                 fontSize: 20
                               ),),
                               controlAffinity: ListTileControlAffinity.leading,
@@ -173,28 +158,37 @@ class _IngredientPageState extends State<IngredientPage> {
                         thumbVisibility: true,
                       ),
                     ),
-                    ElevatedButton(
-                      onPressed: () {
-                        // Add your onPressed functionality here
-                      },
-                      child: Text(
-                        "Start Mixing!",
-                        style: TextStyle(
-                          color: Colors.white, // White text color
-                          fontSize: 16, // Adjust font size as needed
-                        ),
-                      ),
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.brown,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(8), // Adjust border radius as needed
-                        ),
-                      ),
-                    )
+
                   ],
                 ),
               ),
             ),
+            Positioned(
+              bottom: 0, // Adjust the value as needed for vertical spacing
+              left: 0,
+              right: 0,
+              child: Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    // Add your onPressed functionality here
+                  },
+                  child: Text(
+                    "Start Mixing!",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16,
+                    ),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.brown,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+
           ],
         ),
       ),
